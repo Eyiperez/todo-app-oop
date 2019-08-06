480d054226a7c05fd1d949f955906d85fe5d052d
Erika Perez <erikaperez@Erikas-MacBook-Air.local>
2019-08-05 20:20:04 -0400

    complted tododata model with get, save, and delete data

--- app.js
+++ app.js
@@ -2,8 +2,18 @@
 class TodoData {
     constructor() {
         this.list = JSON.parse(localStorage.getItem('todolist') || '[]');
-
     }
+
+    save(value) {
+        this.list.unshift(value);
+        localStorage.setItem('todolist', JSON.stringify(this.list));
+    }
+
+    delete(index) {
+        this.list.splice(index, 1);
+        localStorage.setItem('todolist', JSON.stringify(this.list));
+    }
+
 }
 
 const data = new TodoData();
